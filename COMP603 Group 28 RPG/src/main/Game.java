b85b57cdc83ed51518c382fa77c47fcc773945e1
Mark <test@email.com>
2024-03-23 14:50:07 +1300

    Uploaded Comp603-Group-28-RPG

---
+++ COMP603 Group 28 RPG/src/main/Game.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author cqm0237
+ */
+public class Game{
+  private Player player;
+  private int room;
+
+  public Game(){
+    this.player = new Player();
+    this.room = 0;
+  }
+
+  public void start(Player player, int room){
+
+    while (!(player.isDead()) && room <= 5){
+
+      OutputHandler handler = new OutputHandler();
+      
+      System.out.println(handler.printMenu());
+      System.out.println("You are in room " + room);
+      System.out.println(handler.printPlayerStatus(player));
+
+      
+      //print available items, if no item, then can't use
+      //if there is item: prompt 
+      //start combat
+      //combat ends
+      room++;
+    }
+  }
+
+  public void combat(Player player, Monster monster)
+  {
+    
+  }
+
+  public void setPlayer(Player player) {
+    this.player = player;
+  }
+
+  public Player getPlayer() {
+    return this.player;
+  }
+
+  public void setRoom(int room) {
+    this.room = room;
+  }
+  
+  public int getRoom() {
+    return this.room;
+  }
+}
---
+++ COMP603 Group 28 RPG/src/main/Ogre.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+import java.util.Random;
+
+/**
+ *
+ * @author cqm0237
+ */
+public class Ogre extends Monster {
+  // create random for:
+  // health 60-80
+  // attack 5-15
+  public Ogre(Random rand) {
+    super("Ogre", rand.nextInt(20) + 60, rand.nextInt(10) + 5);
+  }
+}
